efa5990596f41d6df460ee2071bfc6595228d610
Andres Ruiz <rb.andres98@gmail.com>
2019-11-27 00:03:52 -0500

    Try Navigation with custom url

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
 
 Future main() async {
-  runApp(new MyApp());
+  runApp(new CygniBrowser());
 }
 
-class MyApp extends StatefulWidget {
+class CygniBrowser extends StatefulWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  _CygniBrowserState createState() => new _CygniBrowserState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _CygniBrowserState extends State<CygniBrowser> {
   InAppWebViewController webView;
   String url = "";
   double progress = 0;
@@ -39,15 +39,40 @@ class _MyAppState extends State<MyApp> {
                 flex: 1,
                 child: Container(
                   color: Colors.teal,
-                  child: GestureDetector(
-                    onTap: (){
-                      webView.loadUrl(url: 'https://lavialactea.website/',headers: {});
-                    },
-                      child: Center(
-                        child: Text(
-                      'Cygni Browser',
-                      style: TextStyle(fontSize: 36, color: Colors.white),
-                    )),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 4,
+                        child: Center(
+                          child: TextField(onChanged: (texto){
+                            url = texto;
+                          },
+                            decoration: InputDecoration(
+                                labelStyle:
+                                    TextStyle(color: Colors.red, fontSize: 36),
+                                labelText: 'Ingrese URL',
+                                border: InputBorder.none,
+                                hintText: 'URL...'),
+                          ),
+                        ),
+                      ),
+                      Expanded(flex:1,child: RaisedButton(child: Text('Ir'),onPressed: (){
+                        webView.loadUrl(url: url);
+                      },),)
+                      /*GestureDetector(
+                        onTap: () {
+                          webView.loadUrl(
+                              url: 'https://lavialactea.website/', headers: {});
+                        },
+                        child: Center(
+                            child: Text(
+                          'Cygni Browser',
+                          style: TextStyle(fontSize: 36, color: Colors.white),
+                        )),
+                      ),*/
+                    ],
                   ),
                 ),
               ),
@@ -64,7 +89,7 @@ class _MyAppState extends State<MyApp> {
                     ? LinearProgressIndicator(value: progress)
                     : Container()),*/
               Expanded(
-                flex: 4,
+                flex: 8,
                 child: Container(
                   child: InAppWebView(
                     initialUrl: "https://flutter.dev/",
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cygni_browser/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(CygniBrowser());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
